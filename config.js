e8fef6ff1decfe2b8563b353ca542032e29fe385
Impuls3iv <sammy.unterman@gmail.com>
2016-10-18 15:35:59 -0400

    changes to config

--- config.js
+++ config.js
@@ -7,16 +7,6 @@ module.exports = {
           api_key: '381594723838769',
           api_secret: 'ydGKnUONgngNRxtzEMSELKP89EU'
         },
-        // sendgridKey: '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'
     }
-    // production: {
-    //     dbUrl: 'mongodb://admin:Wednesday!4@ds031671.mongolab.com:31671/heroku_app33090372',
-    //     //dbUrl: 'mongodb://kevin:Friday!1@candidate.52.mongolayer.com:10664,candidate.53.mongolayer.com:10256/standin?replicaSet=set-560ea98882a2b3b5a5000cab',
-    //     cloudinary: {
-    //       cloud_name: 'dbvkqu6qs',
-    //       api_key: '381594723838769',
-    //       api_secret: 'ydGKnUONgngNRxtzEMSELKP89EU'
-    //     },
-    //     sendgridKey: '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'
-    // }
+
 }
