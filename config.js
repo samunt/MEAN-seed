0954b977452a83cc2c5336dda150917515a6ffee
Impuls3iv <sammy.unterman@gmail.com>
2016-10-18 15:36:13 -0400

    d

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     development: {
         dbUrl: 'mongodb://sam:gQ1MlmfFHTtN@ds035766.mlab.com:35766/scrivedev',
-        //dbUrl: 'mongodb://kevin:Friday!1@candidate.52.mongolayer.com:10664,candidate.53.mongolayer.com:10256/standin?replicaSet=set-560ea98882a2b3b5a5000cab',
         cloudinary: {
           cloud_name: 'dbvkqu6qs',
           api_key: '381594723838769',
