c5f20fa3143f2fe25d99f40ee347b6602d81299c
Impuls3iv <sammy.unterman@gmail.com>
2016-10-18 15:36:39 -0400

    fixed cloud

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
         dbUrl: 'mongodb://sam:gQ1MlmfFHTtN@ds035766.mlab.com:35766/scrivedev',
         cloudinary: {
-          cloud_name: 'dbvkqu6qs',
+          cloud_name: 'dkqu6qs',
           api_key: '381594723838769',
           api_secret: 'ydGKnUONgngNRxtzEMSELKP89EU'
         },
